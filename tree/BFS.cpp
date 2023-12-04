5d6932b156f2651cb1f1a78d9657493cb4f9d4c6
A3cat <1745374539@qq.com>
2023-12-04 19:58:11 +0800

    Tree

--- tree/BFS.cpp
+++ tree/BFS.cpp
@@ -1,6 +1,8 @@
+#include<stdlib.h>
+#include<stdio.h>
 #define MAX_SIZE 20
 
-typedef struct TreeNode
+typedef struct TreeNode//定义一个节点
 {
 	int val;
 	int height;
@@ -8,7 +10,7 @@ typedef struct TreeNode
 	TreeNode* right;
 }TreeNode;
 
-TreeNode* NewTreeNode(int x)
+TreeNode* NewTreeNode(int x)//创建一个新的节点
 {
 	TreeNode* treenode = (TreeNode*)malloc(sizeof(TreeNode));
 	treenode->val = x;
@@ -18,7 +20,7 @@ TreeNode* NewTreeNode(int x)
 	return treenode;
 }
 
-int* BFS(TreeNode* root, int* size)
+int* BFS(TreeNode* root, int* size)//层次遍历（广度优先）
 {
 	int front, rear;
 	int index, * visit;
@@ -46,8 +48,40 @@ int* BFS(TreeNode* root, int* size)
 }
 
 
+void PreOrder(TreeNode* root, int* visit, int* index)//先序遍历
+{
+	if (root != NULL)
+		visit[(*index)++] = root->val;
+	if (root != NULL)
+		PreOrder(root->left, visit, index);
+	if (root != NULL)
+		PreOrder(root->right, visit, index);
+}
+
+void InOrder(TreeNode* root, int* visit, int* index)//中序遍历
+{
+	if (root != NULL)
+		InOrder(root->left, visit, index);
+	if (root != NULL)
+		visit[(*index)++] = root->val;
+	if (root != NULL)
+		InOrder(root->right, visit, index);
+}
+
+void PostOrder(TreeNode* root, int* visit, int* index)//后序遍历
+{
+	if (root != NULL)
+		PostOrder(root->left, visit, index);
+	if (root != NULL)
+		PostOrder(root->right, visit, index);
+	if (root != NULL)
+		visit[(*index)++] = root->val;
+}
+
+
 int main()
 {
+	//树的创建（连接节点）
 	TreeNode* n1 = NewTreeNode(2);
 	TreeNode* n2 = NewTreeNode(1);
 	TreeNode* n3 = NewTreeNode(5);
@@ -62,11 +96,34 @@ int main()
 	n4->right = n5;
 	n1->height++;
 
+
 	int num = 0;
-	int* visit = BFS(n1, &num);
+	int visit_dfs[MAX_SIZE];
+	int index = 0;
+
+	int* visit = BFS(n1, &num);//层次遍历序列
 	for (; *visit != -1; visit++)
 		printf("%d ", *visit);
 	printf("\n");
-	printf("%d", num);
+
+	PreOrder(n1, visit_dfs, &index);//先序遍历序列
+	for (int* i = visit_dfs; *i > 0; i++)
+		printf("%d ", *i);
+	printf("\n");
+	index = 0;
+
+	InOrder(n1, visit_dfs, &index);//中序遍历序列
+	for (int* i = visit_dfs; *i > 0; i++)
+		printf("%d ", *i);
+	printf("\n");
+	index = 0;
+
+
+	PostOrder(n1, visit_dfs, &index);//后序遍历序列
+	for (int* i = visit_dfs; *i > 0; i++)
+		printf("%d ", *i);
+	printf("\n");
+
+	printf("%d", num);//节点数
 	return 0;
 }
